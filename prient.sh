e3675f5152f17eb47c8ef9fa0329a17897e6b45a
Bijay <bijay78082@gmail.com>
2025-10-14 12:19:12 +0530

    updated

--- prient.sh
+++ prient.sh
@@ -8,9 +8,9 @@
 #  Syntex 
 # echo -e "\e[CODEmMESSAGE\e[0m"
 
-echo -e "e[31mHellow World\e[0m"
-echo -e "e[32mHellow World\e[0m"
-echo -e "e[33mHellow World\e[0m"
-echo -e "e[34mHellow World\e[0m"
-echo -e "e[35mHellow World\e[0m"
-echo -e "e[36mHellow World\e[0m"
+echo -e "\e[31mHellow World\e[0m"
+echo -e "\e[32mHellow World\e[0m"
+echo -e "\e[33mHellow World\e[0m"
+echo -e "\e[34mHellow World\e[0m"
+echo -e "\e[35mHellow World\e[0m"
+echo -e "\e[36mHellow World\e[0m"
