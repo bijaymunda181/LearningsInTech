7bda431d41cbf6baaf20de35d07c80fbc5c80a74
bijaymunda181 <bijay78082@gmail.com>
2025-10-14 12:12:35 +0530

    Create prient.sh
    code to change the sentences of the colour

---
+++ prient.sh
@@ -0,0 +1,16 @@
+# Red      31
+# Green    32
+# Yellow   33
+# Blue     34
+# Meganta  35
+# Cyan     36
+
+#  Syntex 
+# echo -e "\e[CODEmMESSAGE\e[0m"
+
+echo -e "e[31mHellow World\e[0m"
+echo -e "e[32mHellow World\e[0m"
+echo -e "e[33mHellow World\e[0m"
+echo -e "e[34mHellow World\e[0m"
+echo -e "e[35mHellow World\e[0m"
+echo -e "e[36mHellow World\e[0m"
