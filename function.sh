2767740ee1985d92b278dd82eb8f13edcc7ddf92
Bijay <bijay78082@gmail.com>
2025-10-17 19:03:42 +0530

    Exit functions

--- function.sh
+++ function.sh
@@ -8,3 +8,16 @@ I comple my gradution from jamshedpur workers college
 greeting
 
 echo Function Exit status - $?
+
+# you declear var in a main program, you can access in function and vice-versa.
+# Function have its own varibles
+
+input() {
+  echo First Argument - $1
+    echo Second Argument - $2
+    echo All Agrument - $*
+    echo No of Argument - $#
+
+}
+
+input abe 1234
