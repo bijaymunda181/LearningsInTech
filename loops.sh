8a2e49df28becd4087dabf7af2a32590de9bef79
Bijay <bijay78082@gmail.com>
2025-10-17 19:30:23 +0530

    loops

--- loops.sh
+++ loops.sh
@@ -6,4 +6,10 @@ a=10
 while [ $a -gt 0 ]; do
   echo Hellow
   a=$(($a-1))
+  #break # This command can break the loop
   done
+
+  #Based on Inputs
+  for comp in frontend catalog user ; do
+    echo Installing Component - $comp
+    done
