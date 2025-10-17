d868e48f294c2d15f4a9f39c11151fd8eee837d5
Bijay <bijay78082@gmail.com>
2025-10-17 18:44:02 +0530

    Variables

--- vaes.sh
+++ vaes.sh
@@ -1,2 +1,15 @@
+#variable
 a=10
 echo a is $a
+
+#special  variables
+# $0 - $N, $*, $#
+
+#substitution varibles
+## command substitution
+DATE=$(date)
+echo today Date is $DATE
+
+##Arithmatic Substitution
+ADD=$((2+2))
+echo ADD of 2+2 = $ADD
