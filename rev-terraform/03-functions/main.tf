cadcf9411abcee8a764f4cdc7910ac7502ed836f
Bijay <bijay78082@gmail.com>
2025-12-25 16:29:05 +0530

    terraform

--- rev-terraform/03-functions/main.tf
+++ rev-terraform/03-functions/main.tf
@@ -1,70 +1,13 @@
-#plain variable
-variable "movie" {
-  default = "Hindi"
-}
-
-output "movie" {
-  value = var.movie
-}
-
-# list variable
-variable "moves" {
-  default = ["Hindi", "English"]
-}
-
-#Access a list Variable, List Variable starts from Zero
-output "movies_first" {
-  value = var.moves[0]
-}
-
-output "movies_second" {
-  value = var.moves[1]
-}
-
-# Map variable, map of map variable
-variable "movies_type" {
+variable "books" {
   default = {
     Hindi = {
-      Total = 100
-      Debt_language = 3
-    }
-    South = {
-      Total = 200
-      Debt_language = 5
+      Total_book = 50 # Number
+      Book_quality = "Good"
+      For_sale = true
     }
   }
 }
 
-output "movies_type_Hindi_total" {
-  value = var.movies_type["Hindi"]["Total"]
+output "Books" {
+  value = "Total_book = ${var.books["Hindi"]["Total_book"]} , Book_quality = ${var.books["Hindi"]["Book_quality"]} , For_sale = ${var.books["Hindi"]["For_sale"]}"
 }
-
-output "movie_type_Hindi_Debt_Language" {
-  value = var.movies_type["Hindi"]["Debt_language"]
-}
-
-output "movie_type_South_total" {
-  value = var.movies_type["South"]["Total"]
-}
-
-output "movie_type_south_Debt_language" {
-  value = var.movies_type["South"]["Debt_language"]
-}
-
-
-variable "movie_details" {
-  default = {
-    Hindi = {
-      Total = 500 # Number
-      Gener = "Action" # String
-      for_sale = true # Boolean
-    }
-  }
-}
-
-# A variable in combination of any other string then it need to be with in ${}
-
-output "movie_detail_Hindi" {
-  value = "Total = ${var.movie_details["Hindi"]["Total"]} , Gener = ${var.movie_details["Hindi"]["Gener"]} , Sale = ${var.movie_details["Hindi"]["for_sale"]}"
-}
-
