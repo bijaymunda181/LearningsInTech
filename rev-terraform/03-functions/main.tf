f9954e5e26fe8a3319628e613963d5108cc2201f
Bijay <bijay78082@gmail.com>
2025-12-25 15:34:01 +0530

    terraform

--- rev-terraform/03-functions/main.tf
+++ rev-terraform/03-functions/main.tf
@@ -1,7 +1,17 @@
+#plain variable
 variable "movie" {
   default = "Hindi"
 }
 
 output "movie" {
   value = var.movie
+}
+
+# list variable
+variable "moves" {
+  default = ["Hindi", "English"]
+}
+
+output "movies" {
+  value = var.moves
 }
