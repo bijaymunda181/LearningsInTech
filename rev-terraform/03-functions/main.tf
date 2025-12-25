5633e4e80fc942132d9d7acf1e2cf832ffa6badf
Bijay <bijay78082@gmail.com>
2025-12-25 16:55:52 +0530

    terraform

--- rev-terraform/03-functions/main.tf
+++ rev-terraform/03-functions/main.tf
@@ -1,16 +1,16 @@
-# variable "books" {
-#   default = {
-#     Hindi = {
-#       Total_book = 50 # Number
-#       Book_quality = "Good"
-#       For_sale = true
-#     }
-#   }
-# }
-#
-# output "Books" {
-#   value = "Total_book = ${var.books["Hindi"]["Total_book"]} , Book_quality = ${var.books["Hindi"]["Book_quality"]} , For_sale = ${var.books["Hindi"]["For_sale"]}"
-# }
+variable "books" {
+  default = {
+    Hindi = {
+      Total_book = 50 # Number
+      Book_quality = "Good"
+      For_sale = true
+    }
+  }
+}
+
+output "Books" {
+  value = "Total_book = ${var.books["Hindi"]["Total_book"]} , Book_quality = ${var.books["Hindi"]["Book_quality"]} , For_sale = ${var.books["Hindi"]["For_sale"]}"
+}
 
 
 variable "country_details" {
