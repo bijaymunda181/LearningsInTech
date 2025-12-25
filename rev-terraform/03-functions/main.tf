54d821a174a0e9c8fe0600eee42917e2a12582d5
Bijay <bijay78082@gmail.com>
2025-12-25 15:45:13 +0530

    terraform

--- rev-terraform/03-functions/main.tf
+++ rev-terraform/03-functions/main.tf
@@ -12,10 +12,23 @@ variable "moves" {
   default = ["Hindi", "English"]
 }
 
+#Access a list Variable, List Variable starts from Zero
 output "movies_first" {
   value = var.moves[0]
 }
 
 output "movies_second" {
   value = var.moves[1]
+}
+
+# Map variable, map of map variable
+variable "movies_type" {
+  default = {
+    Hindi = 2
+    South = 5
+  }
+}
+
+output "movies" {
+  value = var.movies_type
 }
