576b63da41d9a166c7bd4becbc5bebdfad9e40ef
Bijay <bijay78082@gmail.com>
2025-12-25 16:42:31 +0530

    terraform

--- rev-terraform/03-functions/main.tf
+++ rev-terraform/03-functions/main.tf
@@ -1,13 +1,29 @@
-variable "books" {
+# variable "books" {
+#   default = {
+#     Hindi = {
+#       Total_book = 50 # Number
+#       Book_quality = "Good"
+#       For_sale = true
+#     }
+#   }
+# }
+#
+# output "Books" {
+#   value = "Total_book = ${var.books["Hindi"]["Total_book"]} , Book_quality = ${var.books["Hindi"]["Book_quality"]} , For_sale = ${var.books["Hindi"]["For_sale"]}"
+# }
+
+
+variable "country_details" {
   default = {
-    Hindi = {
-      Total_book = 50 # Number
-      Book_quality = "Good"
-      For_sale = true
+    India = {
+      states = 28
+      capital = "New Delhi"
+      Tourist_place = true
+
     }
   }
 }
 
-output "Books" {
-  value = "Total_book = ${var.books["Hindi"]["Total_book"]} , Book_quality = ${var.books["Hindi"]["Book_quality"]} , For_sale = ${var.books["Hindi"]["For_sale"]}"
+output "country_detail" {
+  value = "states = ${var.country_details["India"]["states"]}"
 }
