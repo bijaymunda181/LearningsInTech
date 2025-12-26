4e8974354bc72eff4153cf0b9794ad5e90815495
Bijay <bijay78082@gmail.com>
2025-12-26 23:38:27 +0530

    terraform

---
+++ rev-terraform/06-loops/for_loop/main.tf
@@ -0,0 +1,12 @@
+variable "components" {
+  default = {
+    catalogue = {name = "catalogue_sg1"}
+    mongodb ={name = "mongodb_sg1"}
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  for_each = var.components
+  name = lookup(var.components,each.value["name"], null )
+  }
+}
