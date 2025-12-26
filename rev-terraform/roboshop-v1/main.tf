13c9c00831fd13fb2d005c747d3b171c14b18e06
Bijay <bijay78082@gmail.com>
2025-12-26 22:18:09 +0530

    terraform

--- rev-terraform/roboshop-v1/main.tf
+++ rev-terraform/roboshop-v1/main.tf
@@ -4,7 +4,7 @@ variable "components" {
 
 
 resource "aws_instance""instance" {
-  count = 2
+  count = length(var.components)
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-03df7f4a9485bc781"]
